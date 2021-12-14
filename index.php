19fc17cbeae621b383c85fab8a40ef0e2d10c6eb
kuronuko96GitH <94874857+kuronuko96GitH@users.noreply.github.com>
2021-12-14 17:29:54 +0900

    upd

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
-<?
-echo 'phpのテストです。<br>';
-
-$dbinfo = parse_url(getenv('DATABASE_URL'));
-
-$dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1);
-
-$pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
-var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
-
-phpinfo();
+<?php
+include('_header.php');
+$title = '';
 ?>
+                    <div class="text-center">
+                        <h1 class="mx-auto my-0 text-uppercase">Portfolio</h1>
+                        <h2 class="text-white-50 mx-auto mt-2 mb-5">ゲームが遊べます。</h2>
+                        <a class="btn btn-secondary" href="TypingGame.php">タイピングゲーム</a>
+                        <a class="btn btn-secondary" href="QuizGame.php">英単語クイズゲーム</a>
+                       
+                        <h3 class="text-white-50 mx-auto mt-2">ランキング登録をする場合は、ログインをして下さい。</h3>
+                        <a class="btn btn-primary" href="login.php">ログイン</a>
+                        <h3 class="text-white-50 mx-auto mt-2">テストユーザー：てすと</h3>
+                        <h3 class="text-white-50 mx-auto mt-2">パスワード：test1234</h3>
+                    </div>
+
+<?php
+include('_footer.php');
