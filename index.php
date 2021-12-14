969d4a793178b5b3e7673adf170fcefb1462c414
kuronuko96GitH <94874857+kuronuko96GitH@users.noreply.github.com>
2021-12-14 17:08:51 +0900

    upd

--- index.php
+++ index.php
@@ -8,5 +8,12 @@ $dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1)
 $pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
 var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
 
+echo '$dbinfo_host：'.$dbinfo['host'];
+echo '$dbinfo_path：'.$dbinfo['path'];
+
+echo '$dbinfo_user：'.$dbinfo['user'];
+echo '$dbinfo_pass：'.$dbinfo['pass'];
+
+
 phpinfo();
 ?>
